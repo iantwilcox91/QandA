ae455d29dabd80fc2365dda0a1879a1914de2cd6
ian Wilcox <ian Wilcox>
2016-10-28 15:20:23 -0700

    add test coded data in indexjs

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
+var questions = [{
+  id: 1,
+  content: "Veruca Salt",
+  author: "San Francisco",
+  notes: "Estate"
+  }, {
+  id: 2,
+  content: "Veruca Salt",
+  author: "San Francisco",
+  notes: "Estate"
+  }, {
+  id: 3,
+  content: "Veruca Salt",
+  author: "San Francisco",
+  notes: "Estate"
+  }];
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
 });
