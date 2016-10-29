6785d3c5f50747344525421b7bc455c04fe8b4c9
ian Wilcox <ian Wilcox>
2016-10-28 18:56:49 -0700

    can save new question posts

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,11 @@ export default Ember.Route.extend({
     deleteQuestion(question) {
       question.destroyRecord();
       this.transitionTo('index');
+    },
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
     }
   }
 });
