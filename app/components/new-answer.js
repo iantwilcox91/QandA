a3f41c0ba9e2694ca032c0d5106d218c8b67de9e
ian Wilcox <ian Wilcox>
2016-11-04 16:52:24 -0700

    add answer rating value - can sort by rating

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -9,6 +9,7 @@ export default Ember.Component.extend({
     saveAnswer() {
      var params = {
        author: this.get('author'),
+       rating: parseInt(this.get('rating')),
        content: this.get('content'),
        question: this.get('question')
      };
