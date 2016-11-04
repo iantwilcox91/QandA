edb057e2400527db01fba327b6b6ed44ccd36811
ian Wilcox <ian Wilcox>
2016-11-04 14:11:36 -0700

    hide button

--- app/helpers/answer-popularity.js
+++ app/helpers/answer-popularity.js
@@ -3,17 +3,17 @@ import Ember from 'ember';
 export function answerPopularity(params) {
   var question = params[0];
 
-  if(question.get('answers').get('length') < 2) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-thumbs-down"></span>');
-  }
-  if(question.get('answers').get('length') >= 2) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-thumbs-up"></span>');
+  if(question.get('answers').get('length') >= 6) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
   }
   if(question.get('answers').get('length') >= 4) {
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-hand-up"></span>');
   }
-  if(question.get('answers').get('length') >= 6) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+  if(question.get('answers').get('length') >= 2) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-thumbs-up"></span>');
+  }
+  if(question.get('answers').get('length') < 2) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-thumbs-down"></span>');
   }
 
 }
